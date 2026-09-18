1e5b6ed9640b78ddbb4751643a1ed34add9da27b
Fabiana <fabiana.rabelog@gmail.com>
2026-09-17 20:41:48 -0400

    Començando a biblioteca

--- Aula08/avaliacao01-com-while.py
+++ Aula08/avaliacao01-com-while.py
@@ -1,7 +1,6 @@
 # PROVA
 from datetime import datetime
 
-biblioteca = []
 
 ano_atual = datetime.now().year
 
---
+++ Aula08/biblioteca.py
@@ -0,0 +1,13 @@
+biblioteca = []
+
+while True:
+
+    print("\n=====SISTEMA DE BIBLIOTECA=====")
+    print("1 - Cadrastrar livro")
+    print("2 - Listar livros")
+    print("3 - Pesquisar livro")
+    print("4 - Alterar livro")
+    print("5 - Excluir livro")
+    print("6 - Sair")
+
+    opcao = input("Escolha uma opação: ")
