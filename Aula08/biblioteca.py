9ba23bf1d36f0faa6a9fefa14960c5d8e8326011
Fabiana <fabiana.rabelog@gmail.com>
2026-09-21 20:09:54 -0400

    Biblioteca sendo editado

--- Aula08/biblioteca.py
+++ Aula08/biblioteca.py
@@ -3,19 +3,20 @@ biblioteca = []
 while True:
 
     print("\n=====SISTEMA DE BIBLIOTECA=====")
-    print("1 - Cadrastrar livro")
-    print("2 - Listar livros")
-    print("3 - Pesquisar livro")
+    print("1 - Pesquisar livro")
+    print("2 - Cadrastrar livro")
+    print("3 - Listar livros")
     print("4 - Alterar livro")
     print("5 - Excluir livro")
     print("6 - Sair")
 
-    opcao = input("Escolha uma opação: ")
-    
-#cadastrar
-
+    opcao = input("Escolha uma opção: ")
+#BUSCA
     if opcao == "1":
-        
+        pass
+    
+#CHAMADA 
+    elif opcao == "2":        
         codigo = int(input("Código do livro: "))
         
         existe = False
@@ -26,4 +27,27 @@ while True:
         
         if existe:
             print("Código já cadasdrado!")
-        
+        else:
+            titulo = input("Título do livro: ")
+            autor = input("Autor do livro: ")
+            ano_publicacao = int(input("Ano de publicação: "))
+
+            livro_adicionado = {"Título": titulo, "Autor": autor, "Ano": ano_publicacao}
+            biblioteca.append(livro_adicionado)
+
+            print("Livro adicionado com sucesso!")
+
+#EDIDIÇÃO DA BIBLIOTECA       
+    elif opcao == "3":
+        print(biblioteca)   
+
+    elif opcao == "4":
+        pass
+
+    elif opcao == "5":
+        pass
+    elif opcao == "6":
+        break
+    else:
+        print("Opção inválida!")
+
